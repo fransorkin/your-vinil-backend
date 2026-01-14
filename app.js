ab6765b73b2c3d77c0e66ed71a8300b77ef26941
fransorkin <sorkinfrancisco@gmail.com>
2026-01-15 00:03:30 +0100

    Añadido modelos , rutas , API

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
-// ℹ️ Gets access to environment variables/settings
-// https://www.npmjs.com/package/dotenv
+// Gets access to environment variables/settings
 require("dotenv").config();
 
-// ℹ️ Connects to the database
+// Connects to the database
 require("./db");
 
 // Handles http requests (express is node js framework)
-// https://www.npmjs.com/package/express
 const express = require("express");
 
 const app = express();
 
-// ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
+// This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
-// 👇 Start handling routes here
+// Start handling routes here
 const indexRoutes = require("./routes/index.routes");
 app.use("/api", indexRoutes);
 
 const authRoutes = require("./routes/auth.routes");
-app.use("/auth", authRoutes);
+app.use("/api/auth", authRoutes);
 
-// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
+const vinylRoutes = require("./routes/vinyls.routes");
+app.use("/api/vinyls", vinylRoutes);
+
+// To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
 module.exports = app;
